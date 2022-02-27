d96286fc3eac985369bb53a668e3c8495b2c54f4
eugeniacarbo <ecarbo16@gmail.com>
2022-02-27 14:56:41 -0300

    Add files via upload

---
+++ js2/js/2pagina.js
@@ -0,0 +1,52 @@
+let nombre = prompt("Holaa, como es tu nombre?");
+
+
+let edad = parseInt(prompt("Hola " + nombre + " Cuantos años tienes?"));
+
+
+
+if (edad < 18) {
+    alert("Este sitio solo acepta mayores de edad.");
+
+} else if (edad >= 18 && edad <= 100) {
+    alert("Ya estas a un paso de tus Vacaciones!!!");
+
+} else {
+    alert("Por favor indicanos tu edad para continuar tu reserva.");
+}
+
+alert("Tnemos 2 cabañas, cada una tiene capacidad para 6 personas");
+
+let cantidad = prompt("Cuantas personas van a ser?");
+
+
+if (cantidad <= 6) {
+    alert("Tu estadia saldrá $9000 por noche.");
+} else if (cantidad > 6 && cantidad < 13) {
+    alert("Vas a necesitar dos cabañas, tu estadia saldrá $18000 por noche");
+} else if (capacidad > 12) {
+    alert("No contamos con esa capacidad");
+}
+
+
+let noches = 40;
+let totalReservas = 0;
+
+
+while (noches >= 40) {
+    totalReservas = parseInt(prompt("Cuantas noches desas reservar?"));
+    if (totalReservas > noches) {
+        alert("No disponemos de tantas noches");
+    }
+    noches = noches - totalReservas;
+
+    console.log(`Queda un total de ${noches} disponibles`);
+}
+alert(`Queda un total de ${noches} noches disponibles.`);
+
+
+let costoPorNochePorPersona = 1500;
+
+costo = costoPorNochePorPersona * totalReservas * cantidad;
+
+alert(`Su reserva tendrá un costo de $ ${costo} `)
